67f69bca9869a2c44b955a8eabb703074c9e6a6e
SunlayGGX <sunlay.g@free.fr>
2016-10-08 13:18:31 -0400

    Object Camera Squeletton
    Not tested
    
    An object camera. Just a squeletton

---
+++ Project/Camera/Camera/ObjectCamera.h
@@ -0,0 +1,23 @@
+/*
+Created by Sun-lay Gagneux
+*/
+#ifndef OBJECTCAMERA_H
+#define OBJECTCAMERA_H
+
+
+#include "AttachedCamera.h"
+
+
+namespace pirateSimulator
+{
+	namespace cameraModule
+	{
+		class ObjectCamera : public AttachedCamera
+		{
+			//...
+		};
+	}
+}
+
+
+#endif //!OBJECTCAMERA_H
