44d1a3ddecc032986a57b301b173e61011dc156d
SunlayGGX <sunlay.g@free.fr>
2016-10-08 13:16:54 -0400

    Free Camera
    Not tested
    
    A free camera. Squeletton

---
+++ Project/Camera/Camera/FreeCamera.h
@@ -0,0 +1,21 @@
+/*
+Created by Sun-lay Gagneux
+*/
+#ifndef FREECAMERA_H
+#define FREECAMERA_H
+
+#include "BaseCamera.h"
+
+namespace pirateSimulator
+{
+	namespace cameraModule
+	{
+		class FreeCamera : public BaseCamera
+		{
+			//...
+		};
+	}
+}
+
+
+#endif //!FREECAMERA_H
