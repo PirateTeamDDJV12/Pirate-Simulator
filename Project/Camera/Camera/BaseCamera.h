c224c1d25c460cfb13702a60948b3f64b4d6d1a6
SunlayGGX <sunlay.g@free.fr>
2016-10-08 13:06:01 -0400

    Base Camera
    Not tested yet
    
    Just an preemptive base declaration. I need it to make my own structures.
    Sorry Lloyd.

---
+++ Project/Camera/Camera/BaseCamera.h
@@ -0,0 +1,17 @@
+#ifndef BASECAMERA_H
+#define BASECAMERA_H
+
+#include "Component.h"
+
+namespace pirateSimulator
+{
+	namespace cameraModule
+	{
+		class BaseCamera : public Component
+		{
+			/* It's for you LLoyd */
+		};
+	}
+}
+
+#endif //!BASECAMERA_H
