7ed72bfa8cadd9f6e1ce4d1b9fdf3a0443e8a623
SunlayGGX <sunlay.g@free.fr>
2016-10-08 13:14:47 -0400

    Level camera
    not tested
    
    Just an ebauche structure

---
+++ Project/Camera/Camera/LevelCamera.h
@@ -0,0 +1,23 @@
+/*
+Created by Sun-lay Gagneux
+*/
+#ifndef LEVELCAMERA_H
+#define LEVELCAMERA_H
+
+
+#include "AttachedCamera.h"
+
+
+namespace pirateSimulator
+{
+	namespace cameraModule
+	{
+		class LevelCamera : public AttachedCamera
+		{
+			//...
+		};
+	}
+}
+
+
+#endif //!LEVELCAMERA_H
