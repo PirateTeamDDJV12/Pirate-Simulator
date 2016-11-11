29c1cb97c3ac9864981055ee6473234815460968
SunlayGGX <sunlay@evotion.fr>
2016-11-10 20:57:55 -0500

    RendererManager class added to project
    Squeletton. functional
    
    just an prototype of the class. Nothing was done and the class is still
    empty.

---
+++ Project/PirateSimulator/RendererManager.h
@@ -0,0 +1,12 @@
+#ifndef RENDERERMANAGER_H_INCLUDED
+#define RENDERERMANAGER_H_INCLUDED
+
+namespace PirateSimulator
+{
+    class RendererManager
+    {
+
+    };
+}
+
+#endif // !RENDERERMANAGER_H_INCLUDED
