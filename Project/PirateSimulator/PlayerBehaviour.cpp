a9ed8942c5172b108c22df4e9890fefd680b8501
Alexandre Sendra <sendra.alexandre@gmail.com>
2016-11-14 16:45:10 -0500

    Replaced ship movements, added camera rotation around the ship

---
+++ Project/PirateSimulator/PlayerBehaviour.cpp
@@ -0,0 +1,120 @@
+#include "StdAfx.h"
+
+#include "PlayerBehaviour.h"
+#include "../PetitMoteur3D/PetitMoteur3D/MoteurWindows.h"
+#include <dinput.h>
+
+using namespace PirateSimulator;
+using namespace DirectX;
+
+PlayerBehaviour::PlayerBehaviour() : m_speed{ 1.0f }, m_cameraRef{ CameraManager::singleton.getMainCameraGO() }
+{
+}
+
+void PlayerBehaviour::move(Move::Translation::Direction direction)
+{
+    Transform transform = m_gameObject->m_transform;
+
+    // Change the boat forward to match camera forward 
+    GameObjectRef camera = CameraManager::singleton.getMainCameraGO();
+    m_gameObject->m_transform.m_forward.vector4_f32[0] = camera->m_transform.m_forward.vector4_f32[0];
+    m_gameObject->m_transform.m_forward.vector4_f32[1] = 0.0f;
+    m_gameObject->m_transform.m_forward.vector4_f32[2] = camera->m_transform.m_forward.vector4_f32[2];
+   
+    m_gameObject->m_transform.m_forward = XMVector3Normalize(m_gameObject->m_transform.m_forward);
+
+    switch (direction)
+    {
+    case Move::Translation::FORWARD:
+    {
+        m_gameObject->translate(m_gameObject->m_transform.m_forward * m_speed);
+    }
+    break;
+
+    case Move::Translation::BACKWARD:
+    {
+        m_gameObject->translate(-m_gameObject->m_transform.m_forward * m_speed);
+    }
+    break;
+
+    case Move::Translation::LEFT:
+    {
+        m_gameObject->translate(-transform.m_right * m_speed);
+
+    }
+    break;
+
+    case Move::Translation::RIGHT:
+    {
+        m_gameObject->translate(transform.m_right * m_speed);
+    }
+    break;
+
+    default:
+        return;
+    }
+}
+
+void PlayerBehaviour::rotate(Move::Rotation::Direction direction)
+{
+
+}
+
+void PlayerBehaviour::anime(float ellapsedTime)
+{
+    // Pour les mouvements, nous utilisons le gestionnaire de saisie
+    PM3D::CMoteurWindows& rMoteur = PM3D::CMoteurWindows::GetInstance();
+    CDIManipulateur& rGestionnaireDeSaisie = rMoteur.GetGestionnaireDeSaisie();
+
+    if (rGestionnaireDeSaisie.ToucheAppuyee(DIK_CAPSLOCK))
+    {
+        m_speed = 5.0f;
+    }
+
+    /*
+    * Rotation
+    */
+    if (rGestionnaireDeSaisie.ToucheAppuyee(DIK_LEFT))
+    {
+        rotate(Move::Rotation::Y_CLOCKWISE);
+    }
+
+    if (rGestionnaireDeSaisie.ToucheAppuyee(DIK_RIGHT))
+    {
+        rotate(Move::Rotation::Y_INVERT_CLOCKWISE);
+    }
+
+    if (rGestionnaireDeSaisie.ToucheAppuyee(DIK_UP))
+    {
+        rotate(Move::Rotation::X_INVERT_CLOCKWISE);
+    }
+
+    if (rGestionnaireDeSaisie.ToucheAppuyee(DIK_DOWN))
+    {
+        rotate(Move::Rotation::X_CLOCKWISE);
+    }
+
+
+    /*
+    * Translation
+    */
+    if (rGestionnaireDeSaisie.ToucheAppuyee(DIK_A))
+    {
+        move(Move::Translation::LEFT);
+    }
+
+    if (rGestionnaireDeSaisie.ToucheAppuyee(DIK_D))
+    {
+        move(Move::Translation::RIGHT);
+    }
+
+    if (rGestionnaireDeSaisie.ToucheAppuyee(DIK_W))
+    {
+        move(Move::Translation::FORWARD);
+    }
+
+    if (rGestionnaireDeSaisie.ToucheAppuyee(DIK_S))
+    {
+        move(Move::Translation::BACKWARD);
+    }
+}
