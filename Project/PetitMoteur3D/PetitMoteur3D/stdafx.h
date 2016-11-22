e712e3419512746a5879a92cf9cd6f55d8bd4ca0
USHERBROOKE\bono1801 <olivier.bonnemains@usherbrooke.ca>
2016-11-22 11:16:22 -0500

    intégration de physics

--- Project/PetitMoteur3D/PetitMoteur3D/stdafx.h
+++ Project/PetitMoteur3D/PetitMoteur3D/stdafx.h
@@ -46,3 +46,25 @@
 #include <d3dcompiler.h>
 
 #include <dinput.h>
+
+//Fichier PhysX
+#include "targetver.h"
+#include <cassert>
+#include "PhysX/Include/PxPhysicsAPI.h"
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+#define SAFE_RELEASE(_x) { (_x)->Release(); (_x) = nullptr; }
+
+#ifdef _DEBUG
+#define ASSERT(x) assert(x)
+#else
+#define ASSERT(x) do { } while (false)
+#endif
+
+#ifndef _NOPCH
+
+// TODO: reference additional headers your program requires here
+
+#endif // _NOPCH
+
