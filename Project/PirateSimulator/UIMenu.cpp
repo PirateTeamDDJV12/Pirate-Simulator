ea761f8c01d388c1213498cc0e444a0341022526
SunlayGGX <sunlay@evotion.fr>
2016-12-04 16:31:56 -0500

    UIBase done
    Tested. No crash but does nothing
    
    UIMenu implemented but does nothing. UIElementBase and UIBase done.

---
+++ Project/PirateSimulator/UIMenu.cpp
@@ -0,0 +1,27 @@
+#include "UIMenu.h"
+
+using namespace PirateSimulator;
+
+UIMenu::UIMenu()
+{}
+
+void UIMenu::initialize()
+{}
+
+bool UIMenu::update()
+{
+    //TODO
+    return true;
+}
+
+bool UIMenu::select()
+{
+    //TODO
+    return true;
+}
+
+bool UIMenu::cancel()
+{
+    //TODO
+    return true;
+}
