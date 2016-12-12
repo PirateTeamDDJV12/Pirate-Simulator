e0cbdad1b02f05cf83596504a4e442e110f68483
Constant Brunel <constant.95@gmail.com>
2016-12-12 18:47:02 -0500

    Add some visibility to understand

--- Project/PirateSimulator/VehicleShape.cpp
+++ Project/PirateSimulator/VehicleShape.cpp
@@ -21,7 +21,7 @@ class CollisionVehicleHandler : public ICollisionHandler
     {
         // auto actor0 = static_cast<ShapeComponent*>(contactShape->getActor()->userData);
         //auto actor1 = static_cast<ShapeComponent*>(actorShape->getActor()->userData);
-        
+
     }
 };
 
@@ -31,16 +31,19 @@ void VehicleShape::setGameObject(GameObject* parent)
     DirectX::XMVECTOR position = parent->m_transform.getPosition();
     physx::PxVec3 newPos(position.vector4_f32[0], position.vector4_f32[1], position.vector4_f32[2]);
     // 			_pxActor->addForce(frontVector);
-	m_actor = PhysicsManager::singleton.physics().createRigidDynamic(physx::PxTransform::createIdentity());
-	m_shape = m_actor->createShape(physx::PxBoxGeometry(10.f, 15.f, 20.f), *m_material); 
+
+    m_actor = PhysicsManager::singleton.physics().createRigidDynamic(physx::PxTransform::createIdentity());
+    m_shape = m_actor->createShape(physx::PxBoxGeometry(10.f, 15.f, 20.f), *m_material);
+
     auto boatPose = pose();
     boatPose.p = newPos;
     setPose(boatPose);
-   
+
     PxFilterData filterData;
-    filterData.word0 = EACTORVEHICLE ;
+    filterData.word0 = EACTORVEHICLE;
     filterData.word1 = EACTORPIECE | EACTORTERRAIN;
     m_shape->setSimulationFilterData(filterData);
+
     setHandler(ICollisionHandlerRef(new CollisionVehicleHandler));
     m_actor->userData = parent;
 
