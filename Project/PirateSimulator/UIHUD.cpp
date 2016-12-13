4b1e9bbf3425ab53c4b3ebf424753de4471a6493
Constant Brunel <constant.95@gmail.com>
2016-12-13 00:16:19 -0500

    Fix hud police and position

--- Project/PirateSimulator/UIHUD.cpp
+++ Project/PirateSimulator/UIHUD.cpp
@@ -5,8 +5,8 @@
 PirateSimulator::UIHUD::UIHUD() : GameObject(Transform(), "HUDGO")
 {
     // Create the police
-    const FontFamily oFamily(L"Arial", NULL);
-    m_police = new Font(&oFamily, 20.00, FontStyleBold, UnitPixel);
+    const FontFamily oFamily(L"Edwardian Script ITC", NULL);
+    m_police = new Font(&oFamily, 40.00, FontStyleBold, UnitPixel);
 
     // Create afficheur sprite
     m_afficheurSprite = new PM3D::CAfficheurSprite();
@@ -16,18 +16,18 @@ PirateSimulator::UIHUD::UIHUD() : GameObject(Transform(), "HUDGO")
     m_speed->Ecrire(L"0 noeud");
 
     // Add score text
-    m_score = new PM3D::CAfficheurTexte(50, 50, m_police, Gdiplus::Color(255, 255, 255, 255));
-    m_score->Ecrire(L"0");
+    m_score = new PM3D::CAfficheurTexte(250, 50, m_police, Gdiplus::Color(255, 255, 255, 255));
+    m_score->Ecrire(L"Score : 0");
 
     // Add time text
-    m_time = new PM3D::CAfficheurTexte(150, 50, m_police, Gdiplus::Color(255, 255, 255, 255));
+    m_time = new PM3D::CAfficheurTexte(250, 50, m_police, Gdiplus::Color(255, 255, 255, 255));
     std::chrono::milliseconds timeFromStart = TimeManager::GetInstance().getTimeFromStart();
     m_time->Ecrire(TimeUtils::timeToString(timeFromStart));
 
     // Add all text to the afficheur sprite
-    m_afficheurSprite->AjouterSpriteTexte(m_speed->GetTextureView(), 850, 780);
-    m_afficheurSprite->AjouterSpriteTexte(m_score->GetTextureView(), 50, 50);
-    m_afficheurSprite->AjouterSpriteTexte(m_time->GetTextureView(), 480, 50);
+    m_afficheurSprite->AjouterSpriteTexte(m_speed->GetTextureView(), 840, 770);
+    m_afficheurSprite->AjouterSpriteTexte(m_score->GetTextureView(), 0, 50);
+    m_afficheurSprite->AjouterSpriteTexte(m_time->GetTextureView(), 430, 50);
 
     // Add the mesh to our game object
     this->addComponent<PirateSimulator::IMesh>(m_afficheurSprite);
